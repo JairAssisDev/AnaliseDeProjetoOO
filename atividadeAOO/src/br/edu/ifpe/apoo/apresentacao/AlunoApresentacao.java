f4c584efe1b9afcab9f96e113e657eaaf62fc6dc
JairAssisDev <63867699+JairAssisDev@users.noreply.github.com>
2023-11-02 21:56:38 -0300

    Add files via upload

--- atividadeAOO/src/br/edu/ifpe/apoo/apresentacao/AlunoApresentacao.java
+++ atividadeAOO/src/br/edu/ifpe/apoo/apresentacao/AlunoApresentacao.java
@@ -9,155 +9,159 @@ import br.edu.ifpe.apoo.excecoes.TamanhoCPFInvalidoException;
 import br.edu.ifpe.apoo.excecoes.TamanhoNomeInvalidoException;
 import br.edu.ifpe.apoo.negocio.FachadaDeNegocioFactory;
 import br.edu.ifpe.apoo.negocio.IFachadaDeNegocio;
+import br.edu.ifpe.apoo.negocio.ValidadorCPF;
 import br.edu.ifpe.apoo.negocio.ValidadorCPFAdapter;
 
 public class AlunoApresentacao {
 
-    public void exibirMenu() throws ExcecaoAlunoInvalido, TamanhoNomeInvalidoException, TamanhoCPFInvalidoException {
-        boolean continuar = true;
-        Scanner scanner = new Scanner(System.in);
-        IFachadaDeNegocio fachadaNegocio = FachadaDeNegocioFactory.getInstancia();
-        
-        while (continuar) {
-            System.out.println("Digite a opção desejada:");
-            System.out.println("1 para inserir um aluno;");
-            System.out.println("2 para consultar um aluno;");
-            System.out.println("3 para remover um aluno;");
-            System.out.println("4 para alterar um aluno;");
-            System.out.println("5 para sair;");
+	public void exibirMenu() throws ExcecaoAlunoInvalido, TamanhoNomeInvalidoException, TamanhoCPFInvalidoException {
+		boolean continuar = true;
+		Scanner scanner = new Scanner(System.in);
+		IFachadaDeNegocio fachadaNegocio = FachadaDeNegocioFactory.getInstancia();
 
-            int opcao = Integer.parseInt(scanner.nextLine());
-            switch (opcao) {
-                case 1:
-                    inserir(fachadaNegocio);
-                    break;
-                case 2:
-                    consultar(fachadaNegocio);
-                    break;
-                case 3:
-                    remover(fachadaNegocio);
-                    break;
-                case 4:
-                    atualizar(fachadaNegocio);
-                    break;
-                case 5:
-                    continuar = false; // Sair do loop
-                    break;
-                default:
-                    System.out.println("Opção inválida. Tente novamente.");
-                    break;
-            }
-        }
-    }
+		while (continuar) {
+			System.out.println("Digite a opção desejada:");
+			System.out.println("1 para inserir um aluno;");
+			System.out.println("2 para consultar um aluno;");
+			System.out.println("3 para remover um aluno;");
+			System.out.println("4 para alterar um aluno;");
+			System.out.println("5 para sair;");
 
-    private void inserir(IFachadaDeNegocio negocio) {
-        ValidadorCPFAdapter validadorCPFAdapter = new ValidadorCPFAdapter();
-        AlunoBuilder alunoBuilder = new AlunoBuilder();
-        Scanner scanner = new Scanner(System.in);
+			int opcao = Integer.parseInt(scanner.nextLine());
+			switch (opcao) {
+			case 1:
+				inserir(fachadaNegocio);
+				break;
+			case 2:
+				consultar(fachadaNegocio);
+				break;
+			case 3:
+				remover(fachadaNegocio);
+				break;
+			case 4:
+				atualizar(fachadaNegocio);
+				break;
+			case 5:
+				continuar = false; // Sair do loop
+				break;
+			default:
+				System.out.println("Opção inválida. Tente novamente.");
+				break;
+			}
+		}
+	}
 
-        try {
-            System.out.println("Digite o nome do aluno:");
-            String nome = scanner.nextLine();
-            alunoBuilder.setNome(nome);
+	private void inserir(IFachadaDeNegocio negocio) {
 
-            System.out.println("Digite o CPF do aluno:");
-            String cpf = scanner.nextLine();
-            boolean teste = validadorCPFAdapter.validarCPF(cpf);
+		ValidadorCPF validadorCPFAdapter = new ValidadorCPFAdapter();
+		AlunoBuilder alunoBuilder = new AlunoBuilder();
+		Scanner scanner = new Scanner(System.in);
 
-            if (teste) {
-                alunoBuilder.setCpf(cpf);
+		System.out.println("Digite o nome do aluno:");
+		String nome = scanner.nextLine();
+		try {
+			alunoBuilder.setNome(nome);
+		} catch (TamanhoNomeInvalidoException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		boolean cpfV = false;
+		String cpf = null;
+		while (cpfV == false) {
 
-                System.out.println("Digite o e-mail do aluno:");
-                String email = scanner.nextLine();
-                alunoBuilder.setEmail(email);
+			System.out.println("Digite o CPF do aluno:");
+			cpf = scanner.nextLine();
+			cpfV = validadorCPFAdapter.validarCPF(cpf);
+			if(!cpfV) {
+				System.out.println("Digite o CPF valido");
+			}
+		}
+		try {
+			alunoBuilder.setCpf(cpf);
+		} catch (TamanhoCPFInvalidoException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println("Digite o e-mail do aluno:");
+		String email = scanner.nextLine();
+		alunoBuilder.setEmail(email);
 
-                Aluno aluno = alunoBuilder.build();
+		Aluno aluno = alunoBuilder.build();
 
-                try {
-                    negocio.inserirAluno(aluno);
-                    System.out.println("Aluno inserido com sucesso. ID: " + aluno.getId());
-                } catch (ExcecaoAlunoInvalido ex) {
-                    System.out.println(ex.getMessage());
-                }
-            } else {
-                System.out.println("CPF Inválido");
-                inserir(negocio);
-            }
-        } catch (TamanhoNomeInvalidoException e) {
-            System.out.println(e.getMessage());
-            inserir(negocio);
-        } catch (TamanhoCPFInvalidoException e) {
-            System.out.println(e.getMessage());
-            inserir(negocio);
-        }
-    }
+		try {
+			negocio.inserirAluno(aluno);
+			System.out.println("Aluno inserido com sucesso. ID: " + aluno.getId());
+		} catch (ExcecaoAlunoInvalido ex) {
+			System.out.println(ex.getMessage());
+		}
 
+	}
 
-    private void consultar(IFachadaDeNegocio negocio) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Digite o ID do aluno que deseja consultar:");
+	private void consultar(IFachadaDeNegocio negocio) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Digite o ID do aluno que deseja consultar:");
 
-        long id = Long.parseLong(scanner.nextLine());
+		long id = Long.parseLong(scanner.nextLine());
 
-        try {
-            Aluno aluno = negocio.getAluno(id);
+		try {
+			Aluno aluno = negocio.getAluno(id);
 
-            if (aluno != null) {
-                System.out.println("Nome: " + aluno.getNome());
-                System.out.println("CPF: " + aluno.getCpf());
-                System.out.println("E-mail: " + aluno.getEmail());
-            } else {
-                System.out.println("Aluno com ID " + id + " não encontrado.");
-            }
-        } catch (AlunoNaoEncontradoException ex) {
-            System.out.println(ex.getMessage());
-            
-        }
-    }
+			if (aluno != null) {
+				System.out.println("Nome: " + aluno.getNome());
+				System.out.println("CPF: " + aluno.getCpf());
+				System.out.println("E-mail: " + aluno.getEmail());
+			} else {
+				System.out.println("Aluno com ID " + id + " não encontrado.");
+			}
+		} catch (AlunoNaoEncontradoException ex) {
+			System.out.println(ex.getMessage());
 
-    private void remover(IFachadaDeNegocio negocio) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Digite o ID do aluno que deseja remover:");
+		}
+	}
 
-        long id = Long.parseLong(scanner.nextLine());
+	private void remover(IFachadaDeNegocio negocio) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Digite o ID do aluno que deseja remover:");
 
-        try {
-        	negocio.removerAluno(id);
-            System.out.println("Aluno removido com sucesso.");
-        } catch (AlunoNaoEncontradoException ex) {
-            System.out.println(ex.getMessage());
-        }
-    }
+		long id = Long.parseLong(scanner.nextLine());
 
-    private void atualizar(IFachadaDeNegocio negocio) throws TamanhoNomeInvalidoException, TamanhoCPFInvalidoException {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Digite o ID do aluno que deseja atualizar:");
+		try {
+			negocio.removerAluno(id);
+			System.out.println("Aluno removido com sucesso.");
+		} catch (AlunoNaoEncontradoException ex) {
+			System.out.println(ex.getMessage());
+		}
+	}
 
-        long id = Long.parseLong(scanner.nextLine());
+	private void atualizar(IFachadaDeNegocio negocio) throws TamanhoNomeInvalidoException, TamanhoCPFInvalidoException {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Digite o ID do aluno que deseja atualizar:");
 
-        try {
-            Aluno aluno = negocio.getAluno(id);
+		long id = Long.parseLong(scanner.nextLine());
 
-            if (aluno != null) {
-                System.out.println("Digite o novo nome do aluno:");
-                String novoNome = scanner.nextLine();
+		try {
+			Aluno aluno = negocio.getAluno(id);
 
-                System.out.println("Digite o novo CPF do aluno:");
-                String novoCpf = scanner.nextLine();
+			if (aluno != null) {
+				System.out.println("Digite o novo nome do aluno:");
+				String novoNome = scanner.nextLine();
 
-                System.out.println("Digite o novo e-mail do aluno:");
-                String novoEmail = scanner.nextLine();
+				System.out.println("Digite o novo CPF do aluno:");
+				String novoCpf = scanner.nextLine();
 
-                aluno.setNome(novoNome);
-                aluno.setCpf(novoCpf);
-                aluno.setEmail(novoEmail);
+				System.out.println("Digite o novo e-mail do aluno:");
+				String novoEmail = scanner.nextLine();
 
-                negocio.atualizarALUNO(aluno);
+				aluno.setNome(novoNome);
+				aluno.setCpf(novoCpf);
+				aluno.setEmail(novoEmail);
 
-                System.out.println("Aluno atualizado com sucesso!");
-            }
-        } catch (AlunoNaoEncontradoException ex) {
-            System.out.println(ex.getMessage());
-        }
-    }
+				negocio.atualizarALUNO(aluno);
+
+				System.out.println("Aluno atualizado com sucesso!");
+			}
+		} catch (AlunoNaoEncontradoException ex) {
+			System.out.println(ex.getMessage());
+		}
+	}
 }
